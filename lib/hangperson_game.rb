f295948789248933d3d6aef5521ffd602380a483
Ubuntu <ubuntu@ip-172-31-94-245.ec2.internal>
2020-02-14 02:03:05 +0000

    Ready for heroku

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,46 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(letter)
+    
+    if letter == nil or letter == "" or !letter.match(/[A-Za-z]/)
+      throw "Error"
+    end
+    
+    if  @guesses.downcase.include? letter.downcase or  @wrong_guesses.downcase.include? letter.downcase
+      return false
+    end
+    
+    if @word.downcase.include? letter.downcase
+        @guesses += letter
+    else
+        @wrong_guesses += letter
+    end
+  end
+  
+  def word_with_guesses(word=@word, guesses=@guesses)
+    word.gsub(/[^ #{guesses}]/, '-')
+  end
+  
+  def check_win_or_lose
+    
+    if @wrong_guesses.length == 7
+      return :lose
+    end
+    
+    if not word_with_guesses.include? "-"
+      return :win
+    end 
+    
+    return :play
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
